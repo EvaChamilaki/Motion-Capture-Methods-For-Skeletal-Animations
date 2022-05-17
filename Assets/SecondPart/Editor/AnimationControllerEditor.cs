f5b01c1852a0fdec84b52e85ad0fd0c1a5ca237c
Eva <evacham7@gmail.com>
2022-05-17 20:29:12 +0300

    Functionality: Add before anim State (1)
    Created function for when I have to add an animation before another animation which is being immediately connected with the Any State.

--- Assets/SecondPart/Editor/AnimationControllerEditor.cs
+++ Assets/SecondPart/Editor/AnimationControllerEditor.cs
@@ -10,10 +10,10 @@ public class AnimationControllerEditor : Editor
     int selected = 0;
     string[] options = new string[] { };
 
-    private AnimationClip animationAdd;
+    private AnimationClip animationAdd, animationAfter;
     private Animation animationRemove;
     AnimatorStateMachine animStateMach;
-    
+
 
     public override void OnInspectorGUI()
     {
@@ -21,6 +21,7 @@ public class AnimationControllerEditor : Editor
 
 
         List<string> list = new List<string>(options.ToList());
+
 
         AnimatorControllerSingleton acEd = (AnimatorControllerSingleton)target;
 
@@ -31,6 +32,7 @@ public class AnimationControllerEditor : Editor
         AnimatorController controller = acEd.Animator_Controller;
 
         animStateMach = controller.layers[0].stateMachine;
+        var states = animStateMach.states;
 
         acEd.showAnims = EditorGUILayout.Foldout(acEd.showAnims, "Existing Animations", true);
 
@@ -80,8 +82,54 @@ public class AnimationControllerEditor : Editor
         {
             AnimationClip m = new AnimationClip { name = animationAdd.name };
             controller.AddMotion(m).AddExitTransition();
+            //var state = animStateMach.AddState(animationAdd.name);
+            //state.AddExitTransition();
+            //controller.GetStateEffectiveMotion(AnimatorState AnyState);
             AnimatorState add = FindState(controller, animationAdd.name);
             animStateMach.AddAnyStateTransition(add);
+        }
+
+        animationAfter = EditorGUILayout.ObjectField(animationAfter, typeof(AnimationClip), true) as AnimationClip;
+
+        if (GUILayout.Button("eyo"))
+        {
+            foreach (var state in states)
+            {
+                foreach (var s in state.state.transitions)
+                {
+                    if (state.state.name == "StartWalking" && s.isExit)
+                    {
+                        AnimationClip m = new AnimationClip { name = animationAdd.name };
+                        controller.AddMotion(m).AddExitTransition();
+                        AnimatorState animAfter = FindState(controller, animationAfter.name);
+                    }
+                    Debug.Log(state.state.name + " has " + s.isExit);
+                    //state.state.
+                }
+            }
+        }
+
+        if (GUILayout.Button("Tests"))
+        {
+            //animStateMach.RemoveAnyStateTransition(animStateMach.anyStateTransitions[3]);
+            Debug.Log(animStateMach.anyStateTransitions.Length);
+            for (int i = 0; i < animStateMach.anyStateTransitions.Length; i++)
+            {
+                Debug.Log(i + ": " + animStateMach.anyStateTransitions[i].destinationState.name);
+
+                if (animStateMach.anyStateTransitions[i].destinationState.name == animationAfter.name)
+                {
+                    AnimationClip m = new AnimationClip { name = animationAdd.name };
+
+                    AnimatorState animationStateAddBeforeThis = FindState(controller, animationAfter.name);
+                    controller.AddMotion(m).AddTransition(animationStateAddBeforeThis);
+
+                    AnimatorState add = FindState(controller, animationAdd.name);
+                    animStateMach.AddAnyStateTransition(add);
+
+                    animStateMach.RemoveAnyStateTransition(animStateMach.anyStateTransitions[i]);
+                }
+            }
         }
 
 
@@ -104,7 +152,6 @@ public class AnimationControllerEditor : Editor
 
         if (GUILayout.Button("Remove"))
         {
-            //Debug.Log(anim.GetComponents<Animation>());
             //animationRemove.RemoveClip(options[selected]);
             AnimatorState removable = FindState(controller, options[selected]);
             animStateMach.RemoveState(removable);
@@ -114,21 +161,21 @@ public class AnimationControllerEditor : Editor
         }
     }
 
-    public AnimatorState FindState(AnimatorController _animCont, string _stateName)
+    public AnimatorState FindState(AnimatorController animCont, string stateName)
     {
-        for (int i = 0; i < _animCont.layers.Length; i++)
+        for (int i = 0; i < animCont.layers.Length; i++)
         {
-            foreach (var child in _animCont.layers[i].stateMachine.states)
+            foreach (var child in animCont.layers[i].stateMachine.states)
             {
-                if (child.state.name == _stateName)
+                if (child.state.name == stateName)
                 {
-                    Debug.Log("FOund it: " + child.state);
+                    Debug.Log("Found it: " + child.state);
                     //Destroy(child.state);
                     return child.state;
                 }
             }
         }
-        Debug.LogError("Could not find state: " + _stateName + " in: " + _animCont.name);
+        Debug.LogError("Could not find state: " + stateName);
         return null;
     }
 
